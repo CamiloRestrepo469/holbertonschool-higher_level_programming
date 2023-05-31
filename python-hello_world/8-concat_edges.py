c5fa98d14af5ac3e7119112c3071be393b7b9ad7
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-05-31 15:19:59 -0500

    add new proyect

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print("object-oriented programming ", end='')
-print("with Python")
+splits = str.split(" ")
+print(splits[5]+" "+splits[6]+" "+splits[12]+" "+splits[0])
+
