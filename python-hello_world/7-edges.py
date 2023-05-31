e28c13a338bd2a1d2e68d5ad0c58d1ad19d18ca5
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-05-31 09:37:14 -0500

    fix problem

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "Hol"
-word_last_2 = "on"
-middle_word = "olberton"
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+word_first_3 = "Holberton"
+word_last_2 = "Holberton"
+middle_word = "Holberton"
+print(f"First 3 letters: {word_first_3[0:3]}")
+print(f"Last 2 letters: {word_last_2[7:9]}")
+print(f"Middle word: {middle_word[1:8]}")
