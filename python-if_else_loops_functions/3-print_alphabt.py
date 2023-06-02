669b1703e78357f3dcbed3736e1aec8f44557709
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-01 22:48:47 -0500

    fix problem

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    letra = chr(i)
-    if letra.find('e') != -1:
-        continue
-    if letra.find('q') != -1:
-        continue
-    print(letra, end='')
+for code in range(97, 123):
+    if code != 101 and code != 113:
+        print(chr(code), end='')
+
+# for i in range(97, 123):
+#     letra = chr(i)
+#     if letra.find('e') != -1:
+#         continue
+#     if letra.find('q') != -1:
+#         continue
+#     print(letra, end='')
