1750b0ae7966ba2a39e8fb3ffc9e39eff7957430
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-02 11:02:09 -0500

    fix problem format

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    if str == ord('A'):
+        for i in range(ord('A') + 1):
+            print(chr(i), end=' ')
+
+        return True
+        
+    else:
+        return KeyError
