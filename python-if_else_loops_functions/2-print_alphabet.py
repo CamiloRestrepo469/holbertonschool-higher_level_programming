74c2c746d572bf57f623fa11a5c2d5950a5c1540
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-01 20:59:33 -0500

    add  alpha

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# for i in range(26):
+#     print(chr(ord('a') + i))
+
+
+begin = ord('a')
+end = ord('z') + 1
+i = 26
+while i != 97:
+    caracter = chr(i)
+    # Lo que quieras hacer
+    i += 1
+
