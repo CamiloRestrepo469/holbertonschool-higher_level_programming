aa657bcee087e249e61a498ea3a1e551b52eda74
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-02 11:36:11 -0500

    fix problem with ,

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,8 @@
 
 i = 0
 while i <= 99:
-    print("{:d}".format(i), end=', ')
+    if i == 99:
+        print("{:d}".format(i), end='')
+    else:
+        print("{:d}".format(i), end=', ')
     i += 1
