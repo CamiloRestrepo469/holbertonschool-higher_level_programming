7aa28a107c61b2b2ac468a44281db5580cd3ad78
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-03 13:30:30 -0500

    fix problem with name

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
 
-add = __import__('add_0').add
-
-resultado = add(1, 2)
-print("1 + 2 = {:d}\n".format(resultado), end="")
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
