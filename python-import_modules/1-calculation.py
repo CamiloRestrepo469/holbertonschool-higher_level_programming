57e0a717bf18619e17b207273834d35d220f7302
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-03 14:24:17 -0500

    add calculador

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -8,6 +8,9 @@ b = 5
 
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
+if __name__ == "__main__":
     print("{} * {} = {}".format(a, b, mul(a, b)))
+if __name__ == "__main__":
     print("{} / {} = {}".format(a, b, div(a, b)))
