f528c2cb4f7eb0f9644151d3a0f2bce07b34e22f
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-04 12:53:36 -0500

    fix problem with style

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    
-	import sys
 
-	arguments = sys.argv[1:]  # Exclude the script name from the arguments
+    import sys
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
 
-	num_arguments = len(arguments)
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        if num_arguments == 1:
+            print("1 argument:")
+        else:
+            print(f"{num_arguments} arguments:")
 
-	if num_arguments == 0:
-		print("0 arguments.")
-	else:
-		if num_arguments == 1:
-			print("1 argument:")
-		else:
-			print(f"{num_arguments} arguments:")
-
-		for i, arg in enumerate(arguments, start=1):
-			print(f"{i}: {arg}")
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
