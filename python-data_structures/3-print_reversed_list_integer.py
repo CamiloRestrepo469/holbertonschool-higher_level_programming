7f23fed2559dd8a4b976b3b53c481de08efed975
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-05 12:11:59 -0500

    fix problem spacie

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     if my_list[0] == 0:
-        print("{}".format(i))
         return None
     for i in my_list:
         print("{:d}".format(i))
