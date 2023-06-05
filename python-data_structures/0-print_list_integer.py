42695caa120f4e79692495a8df6cd1e29ef5b7b9
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-05 10:21:23 -0500

    add in for

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 
-
-    # Prototipo: def imprimir_lista_integros(mi_lista=[]):
-    # Formato: un entero por línea. Ver ejemplo
-    # No puedes importar ningún módulo
-    # Puedes asumir que la lista sólo contiene enteros
-    # No está permitido convertir enteros en cadenas.
-    # Tienes que usar str.format() para imprimir enteros
-
-
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{}".format(i))
-       
-
-
-
-
+        print("{:d}".format(i))
