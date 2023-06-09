f10fe54d383fc27245b640180b593dc83a55a9f1
Holberton <holberton@Holbertons-iMac.local>
2023-06-09 15:07:31 -0500

    add new file 1 safe

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        # Imprimir el valor como entero
+        # utilizando el formato "{:d}"
+        return True
+    # Devolver True si el valor
+    # es un entero y se imprime correctamente
+    except Exception:
+        # Capturar cualquier excepción que
+        # pueda ocurrir durante la impresión
+        return False
+    # Devolver False si se produce
+    # una excepción, lo que indica que el valor
+    # no es un entero o no se puede imprimir como entero
