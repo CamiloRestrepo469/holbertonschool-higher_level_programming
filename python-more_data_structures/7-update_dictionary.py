46a7ed073f34bc537b7addcaed64b5804f810014
Holberton <holberton@Holbertons-iMac.local>
2023-06-09 08:23:21 -0500

    add new file

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if value is None:
+        a_dictionary[key]
+    if key in a_dictionary:
+        pass
+    if not value:
+        pass
+    if not key:
+        pass
+    a_dictionary[key] = value
+    return a_dictionary
