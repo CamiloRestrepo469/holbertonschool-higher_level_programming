f7d18e839559d2d76baac678ee50d548155547a6
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-12 14:47:06 -0500

    add new file de area

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -29,3 +29,20 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+"""
+En el código anterior, se definen dos tipos de variables:
+
+1. Tipo de variable: `size` es una variable de tipo entero (`int`).
+   - Se utiliza para representar el tamaño del cuadrado.
+
+2. Tipo de variable: `self.__size` es un atributo de instancia de tipo entero (`int`).
+   - Es un atributo privado que almacena el tamaño del cuadrado dentro de la clase `Square`.
+
+Valor de las variables:
+
+- Para la variable `size`, el valor se proporciona como argumento al crear una nueva instancia de la clase `Square`. Puede ser cualquier número entero no negativo.
+- Para el atributo `self.__size`, el valor se asigna dentro del método `set_size`. Es el valor válido proporcionado como argumento a `set_size`. El valor puede ser cualquier número entero no negativo.
+
+Es importante destacar que `size` es una variable local dentro del método `set_size`, mientras que `self.__size` es un atributo de instancia que se puede acceder desde otros métodos dentro de la clase.
+"""
