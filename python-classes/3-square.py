ecd6094a8045064540cf85f9b46177acb30ef32c
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-12 14:49:53 -0500

    add new file de area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""create a class that Square"""
 class Square:
+    """ create instance of Square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
