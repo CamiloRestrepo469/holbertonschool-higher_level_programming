f6b05163b5db15b501e5a3f9640409f34b1d4464
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-15 18:07:39 -0500

    add new file test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,11 +5,26 @@ def print_square(size):
     
      """Print the square using '#' character."""
      if not isinstance(size, int):
+         """isinstance of size and int
+         
+         Raise TypeError if size must be an integer
+         """
          raise TypeError('size must be an integer')
      elif not isinstance(size, int) or size < 0:
+         """isinstance of size and int or size is negative
+         
+         
+         Raise ValueError size must be menor than zero
+         """
          raise ValueError('size must be >= 0')
      elif not isinstance(size, float) and size < 0:
+         """isinstance of size and float or size is negative
+         
+         
+         Raise TypeError size must be an integer
+         """
          raise TypeError(' size must be an integer')
      else:
         for _ in range(size):
+            """print size and # * size"""
             print("#" * size)
