e0d4abe95f45b740cc2cbf9a669a8ef02abe99d4
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-15 16:01:05 -0500

    add new proyect #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""create a function"""
+
+def print_square(size):
+    
+     """Print the square using '#' character."""
+     if not isinstance(size, int):
+         raise TypeError('size must be an integer')
+     elif not isinstance(size, int) or size < 0:
+         raise ValueError('size must be >= 0')
+     elif not isinstance(size, float) and size < 0:
+         raise TypeError(' size must be an integer')
+     else:
+        for _ in range(size):
+            print("#" * size)
