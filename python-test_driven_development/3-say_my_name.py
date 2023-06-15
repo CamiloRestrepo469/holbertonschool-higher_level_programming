ca0dbfc3f490ae63d98b29591501201107134299
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-15 15:14:39 -0500

    fix problem style

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
+"""create functions"""
+
 
 def say_my_name(first_name, last_name=""):
     """_summary_
 
     Args:
-        first_name (_type_): _description_
-        last_name (str, optional): _description_. Defaults to "".
-
-    Raises:
-        TypeError: _description_
-        TypeError: _description_
+        first_name (_type_): call name
+        last_name (str, optional): call last_name. Defaults to "".
     """
     if not isinstance(first_name, str):
         """_summary_
 
         Args:
-            str (_type_): _description_
+            str (_type_): isinstance(first_name, str)
 
         Raises:
-            TypeError: _description_
+            TypeError: first_name must be a string
         """
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
         """_summary_
 
         Args:
-            str (_type_): _description_
+            str (_type_): isinstance(last_name,)
 
         Raises:
-            TypeError: _description_
+            TypeError:last_name must be a string
         """
         raise TypeError('last_name must be a string')
     if first_name:
-        """_summary_
+        """print first_name and last_name
         """
-        print("My name is {} {}".format(first_name,last_name))
+        print("My name is {} {}".format(first_name, last_name))
     else:
-        """_summary_
+        """print Errored first_name and last_name
         """
         print("{}".format(last_name))
