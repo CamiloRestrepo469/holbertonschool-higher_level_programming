e81424ed70ca28f966c97d33338785d8b84c5d16
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-15 18:22:40 -0500

    fix problem space style

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
-"""create a function"""
+"""
+Define print # in multiple lines
+"""
+
 
 def print_square(size):
-    
-     """Print the square using '#' character."""
-     if not isinstance(size, int):
-         """isinstance of size and int
-         
-         Raise TypeError if size must be an integer
-         """
-         raise TypeError('size must be an integer')
-     elif not isinstance(size, int) or size < 0:
-         """isinstance of size and int or size is negative
-         
-         
-         Raise ValueError size must be menor than zero
-         """
-         raise ValueError('size must be >= 0')
-     elif not isinstance(size, float) and size < 0:
-         """isinstance of size and float or size is negative
-         
-         
-         Raise TypeError size must be an integer
-         """
-         raise TypeError(' size must be an integer')
-     else:
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    elif not isinstance(size, int) or size < 0:
+        raise ValueError('size must be >= 0')
+    elif not isinstance(size, float) and size < 0:
+        raise TypeError(' size must be an integer')
+    else:
         for _ in range(size):
-            """print size and # * size"""
             print("#" * size)
