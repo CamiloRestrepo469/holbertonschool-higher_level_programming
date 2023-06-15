b94a92749823b9c4b48683b06553ad88e9b1e4e5
Holberton <holberton@Holbertons-iMac.local>
2023-06-15 08:06:07 -0500

    fix problem is line six

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-    
+    """funtion for adding integer"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
