80fa6345fe2f6bc8afd6fede0ac0dda5fe3acc3c
Holberton <holberton@Holbertons-iMac.local>
2023-06-16 10:05:55 -0500

    fix problem is space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-
+"""_summary_
+    """
 
 def text_indentation(text):
+    """_summary_
+
+    Args:
+        text (_type_): _description_
+
+    Raises:
+        TypeError: _description_
+    """
     if not isinstance(text, str):
+        """_summary_
+
+        Args:
+            text (_type_): _description_
+            str (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+        """
         raise TypeError("text must be a string")
     for i in text:
         if i == '.' or i == '?' or i == ':':
