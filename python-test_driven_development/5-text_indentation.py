440669eaaeb325d1af5827a1946d5eefa01cc333
Holberton <holberton@Holbertons-iMac.local>
2023-06-16 10:21:16 -0500

    add new file text_indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,23 @@
-#!/usr/bin/python3
-"""_summary_
-    """
+#!/usr/bin/python
 
 def text_indentation(text):
-    """_summary_
-
-    Args:
-        text (_type_): _description_
-
-    Raises:
-        TypeError: _description_
-    """
-    if not isinstance(text, str):
-        """_summary_
-
-        Args:
-            text (_type_): _description_
-            str (_type_): _description_
-
-        Raises:
-            TypeError: _description_
-        """
+    if type(text) is not str:
         raise TypeError("text must be a string")
-    for i in text:
-        if i == '.' or i == '?' or i == ':':
-            print("{}".format(i), '\n\n')
-        print(i.strip(), end="")
+
+    punctuation_marks = ['.', '?', ':']
+    for delim in punctuation_marks:
+        text = delim.join([line.strip(" ") for line in text.split(delim)])
+        text = text.replace(delim, delim + "\n\n")
+
+    print(text, end="")
+
+    """
+    En esta versión, utilizamos un bucle for para iterar sobre cada marca de puntuación ('.', '?', ':'). Luego, dividimos el texto en líneas usando split(delim), eliminamos los espacios en blanco de cada línea con strip(" ") y finalmente volvemos a unir las líneas con join() usando la marca de puntuación como separador.
+
+Después de eso, utilizamos replace(delim, delim + "\n\n") para reemplazar cada marca de puntuación por la misma marca de puntuación seguida de dos nuevas líneas.
+
+Finalmente, imprimimos el texto resultante utilizando print(text, end="") para evitar un salto de línea adicional al final.
+
+Esta versión logra el mismo resultado que el código original, pero utiliza una sintaxis ligeramente diferente.
+    
+    """
