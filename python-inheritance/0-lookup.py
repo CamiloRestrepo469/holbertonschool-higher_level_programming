520b2fe575d95637905725d32252ee29b9815ba0
Holberton <holberton@Holbertons-iMac.local>
2023-06-20 14:02:53 -0500

    fix problem space

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """_summary_
-
-    Args:
-        obj (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
     return dir(obj)
