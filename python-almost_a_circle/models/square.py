c3e4320cddbe49a699c0d6965493f17f383c0e08
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-29 10:10:05 -0500

    add code method is update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,3 +27,20 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    """method update"""
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+            
