9871ca81fdd78517521e8449d738f812558741cb
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-06-29 10:14:40 -0500

    fix problem is space

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -34,7 +34,7 @@ class Square(Rectangle):
         """number of arguments"""
         argc = len(args)
         kwargc = len(kwargs)
-        modif_attrs = ['id', 'size', 'x', 'y']
+        modif_attrs = ["id", "size", "x", "y"]
 
         if argc > 5:
             argc = 5
@@ -46,4 +46,3 @@ class Square(Rectangle):
             for k, v in kwargs.items():
                 if k in modif_attrs:
                     setattr(self, k, v)
-    
