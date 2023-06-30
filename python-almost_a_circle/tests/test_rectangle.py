03f7bdb5284e8cf8f6826f8f3545a7cf15ccba26
Holberton <holberton@Holbertons-iMac.local>
2023-06-30 11:50:23 -0500

    add new file tests

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,56 +1,125 @@
 import unittest
 from models.rectangle import Rectangle
+from unittest.mock import patch
+import io
+from io import StringIO
+
 
 class TestRectangle(unittest.TestCase):
 
-    def test_valid_width(self):
-        rectangle = Rectangle(5, 10)
-        self.assertEqual(rectangle.width, 5)
+    def test_rectangle_initialization(self):
+        rect = Rectangle(5, 10)
+        self.assertEqual(rect.width, 5)
+        self.assertEqual(rect.height, 10)
+        self.assertEqual(rect.x, 0)
+        self.assertEqual(rect.y, 0)
+        self.assertIsNotNone(rect.id)
 
-    def test_valid_height(self):
-        rectangle = Rectangle(5, 10)
-        self.assertEqual(rectangle.height, 10)
-
-    def test_invalid_width(self):
-        with self.assertRaises(ValueError):
-            rectangle = Rectangle(-5, 10)
-
-    def test_invalid_height(self):
-        with self.assertRaises(ValueError):
-            rectangle = Rectangle(5, -10)
-
-    def test_invalid_width_type(self):
+    def test_rectangle_width_validation(self):
         with self.assertRaises(TypeError):
-            rectangle = Rectangle("5", 10)
+            rect = Rectangle("invalid", 10)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(0, 10)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-5, 10)
 
-    def test_invalid_height_type(self):
+    def test_rectangle_height_validation(self):
         with self.assertRaises(TypeError):
-            rectangle = Rectangle(5, "10")
+            rect = Rectangle(5, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 0)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, -10)
 
-    def test_area(self):
-        rectangle = Rectangle(5, 10)
-        self.assertEqual(rectangle.area(), 50)
+    def test_rectangle_x_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, 10, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 10, -1)
+
+    def test_rectangle_y_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, 10, 0, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 10, 0, -1)
+
+    def test_rectangle_area(self):
+        rect = Rectangle(4, 5)
+        self.assertEqual(rect.area(), 20)
+        rect.width = 10
+        rect.height = 3
+        self.assertEqual(rect.area(), 30)
+
+    def test_rectangle_str(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        expected_output = "[Rectangle] (1) 2/3 - 4/5"
+        self.assertEqual(str(rect), expected_output)
 
     def test_display(self):
-        rectangle = Rectangle(5, 10)
-        rectangle.display()  # Manually inspect the output
+        rect = Rectangle(4, 5, 2, 3, 1)
+        expected_output = "\n\n\n  ####\n  ####\n  ####\n  ####\n  ####\n"
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            rect.display()
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
-    def test_to_string(self):
-        rectangle = Rectangle(5, 10, id=1, x=2, y=3)
-        expected_string = "[Rectangle] (1) 2/3 - 5/10"
-        self.assertEqual(str(rectangle), expected_string)
+    def test_display_one_arg(self):
+        r = Rectangle(5, 1, 2, 4, 7)
+        with self.assertRaises(TypeError):
+            r.display(1)
+    
+    def test_rectangle_display_without_x_y(self):
+        rectangle = Rectangle(5, 3)
+        expected_output = "#####\n#####\n#####\n"
+        with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_update_args(self):
-        rectangle = Rectangle(5, 10)
-        rectangle.update(2, 7, 15)
-        self.assertEqual(rectangle.width, 7)
-        self.assertEqual(rectangle.height, 15)
+    def test_rectangle_display_without_y(self):
+        rectangle = Rectangle(5, 3, 2)
+        expected_output = "  #####\n  #####\n  #####\n"
+        with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_update_kwargs(self):
-        rectangle = Rectangle(5, 10)
-        rectangle.update(width=7, height=15)
-        self.assertEqual(rectangle.width, 7)
-        self.assertEqual(rectangle.height, 15)
+    def test_rectangle_update_with_args(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        rect.update(2, 6, 7, 1, 4)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 6)
+        self.assertEqual(rect.height, 7)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 4)
+
+    def test_rectangle_update_with_kwargs(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        rect.update(id=2, width=6, height=7, x=1, y=4)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 6)
+        self.assertEqual(rect.height, 7)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 4)
+
+    def test_rectangle_update_with_args_and_kwargs(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        rect.update(2, width=6, height=7, y=4)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 6)
+        self.assertEqual(rect.height, 7)
+        self.assertEqual(rect.x, 2)
+        self.assertEqual(rect.y, 4)
+
+    def test_to_dictionary(self):
+        rect = Rectangle(5, 10, 2, 3, 1)
+        rect_dict = rect.to_dictionary()
+        expected_dict = {
+            "x": 2,
+            "y": 3,
+            "id": 1,
+            "height": 10,
+            "width": 5
+        }
+        self.assertDictEqual(rect_dict, expected_dict)
+
 
 if __name__ == '__main__':
     unittest.main()
