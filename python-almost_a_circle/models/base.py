a960d0f3c14037c88b09750118a6ed273a5e69a7
Holberton <holberton@Holbertons-iMac.local>
2023-06-30 09:39:51 -0500

    fix problem is space in static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,12 +20,11 @@ class Base:
     """static method"""
     @staticmethod
     def to_json_string(list_dictionaries):
-        """create json"""
+        import json
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
-
+        return json.dumps(list_dictionaries)
+    
     """class method"""
     @classmethod
     def save_to_file(cls, list_objs):
