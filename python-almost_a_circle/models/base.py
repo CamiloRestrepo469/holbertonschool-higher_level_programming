c1e167bf724044a663ce4c883705c60d69f6406d
Holberton <holberton@Holbertons-iMac.local>
2023-06-30 09:19:54 -0500

    add new json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""import json"""
+import json
 """create a new Base"""
 
 
@@ -14,6 +16,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    """json"""
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
 
 """
--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -117,8 +117,8 @@ class Rectangle(Base):
         """create dictionary"""
         return {
             "x": self.x,
-            "y": self.y,
+            "width": self.width,
             "id": self.id,
             "height": self.height,
-            "width": self.width,
+            "y": self.y,
         }
